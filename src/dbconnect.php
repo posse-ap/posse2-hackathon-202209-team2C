50393ebbc735cdd1abd82da25fdfa9f499a78c7a
watanabe-miyuki <kirin.myk2018@gmail.com>
2022-09-06 23:57:01 +0900

    feat: dbconnect.php でデフォルトをFETCH_ASSOCに、h()追加

--- src/dbconnect.php
+++ src/dbconnect.php
@@ -6,7 +6,15 @@ $password = 'password';
 try {
   $db = new PDO($dsn, $user, $password);
   $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+  $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false); //SQLインジェクション対策
 } catch (PDOException $e) {
   echo '接続失敗: ' . $e->getMessage();
   exit();
+}
+
+/* htmlspecialchars を短くする */
+function h($value)
+{
+    return htmlspecialchars((string)$value, ENT_QUOTES, "UTF-8");
 }
