1c44fc0f172e0846c5c85dea4681b2fe1feae4c2
watanabe-miyuki <kirin.myk2018@gmail.com>
2022-09-07 23:20:30 +0900

    feat: #32 composerインストール済み

---
+++ github-app-keys.php
@@ -0,0 +1,5 @@
+<?php
+return [
+    'clientId'          => '6cd213e695115a4c2473',
+    'clientSecret'      => 'c5ea33eb3555beb37335f26e1e9fa4f2f320c7e4'
+];
---
+++ index.php
@@ -0,0 +1,13 @@
+<?php require_once '_config.php'; ?>
+<html>
+<?php require_once '_head.php'; ?>
+<body>
+
+<h1><?=$title?></h1>
+
+<p>This is sample application of github login with php.</p>
+
+<a href="/login.php">Login with GitHub</a>
+
+</body>
+</html>
