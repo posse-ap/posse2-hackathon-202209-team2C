d4321f836698c531ee84a595ee32570b2c2a736a
watanabe-miyuki <kirin.myk2018@gmail.com>
2022-09-08 13:56:53 +0900

    コメントアウト追加

--- src/slack.php
+++ src/slack.php
@@ -1,26 +1,5 @@
 <?php
 
-// mb_language('ja');
-// mb_internal_encoding('UTF-8');
-
-// $to = "hackathon-teamX@posse-ap.com";
-// $subject = "PHPからメール送信サンプル";
-// $body = "本文";
-// $headers = ["From"=>"system@posse-ap.com", "Content-Type"=>"text/plain; charset=UTF-8", "Content-Transfer-Encoding"=>"8bit"];
-
-// $name = "テスト";
-// $date = "2021年08月01日（日） 21:00~23:00";
-// $event = "縦モク";
-// $body = <<<EOT
-// {$name}さん
-// ${date}に${event}を開催します。
-// 参加／不参加の回答をお願いします。
-// http://localhost/
-// EOT;
-
-// mb_send_mail($to, $subject, $body, $headers);
-// echo "メールを送信しました";
-
 $headers = [
     'Authorization: Bearer xoxb-1030735452658-2430137901444-RuYFtS6N6zl4uZQSah0CLJ0b', //（1)
     'Content-Type: application/json;charset=utf-8'
@@ -30,7 +9,7 @@ $url = "https://slack.com/api/chat.postMessage"; //(2)
 
 //(3)
 $post_fields = [
-    "channel" => "posse2-hackathon-202209-team2c",
+    "channel" => "#posse2-hackathon-202209-team2c",
     "text" => "初めてのSlack Web APIからのメッセージ",
     "as_user" => true
 ];
@@ -52,3 +31,27 @@ $result = curl_exec($ch);
 curl_close($ch);
 
 echo "メールを送信しました";
+
+// $url = "https://hooks.slack.com/services/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";
+// $message = [
+//   "channel" => "#posse2-hackathon-202209-team2c",
+//   "text" => "メッセージ内容"
+// ];
+
+// $ch = curl_init();
+
+// $options = [
+//   CURLOPT_URL => $url,
+//   // 返り値を文字列で返す
+//   CURLOPT_RETURNTRANSFER => true,
+//   CURLOPT_SSL_VERIFYPEER => false,
+//   // POST
+//   CURLOPT_POST => true,
+//   CURLOPT_POSTFIELDS => http_build_query([
+//     'payload' => json_encode($message)
+//   ])
+// ];
+
+// curl_setopt_array($ch, $options);
+// curl_exec($ch);
+// curl_close($ch);
